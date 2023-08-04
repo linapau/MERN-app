826930ade035b79365d65f91b909c0a3cf77cf1a
Paulina Poręba <p.poreba@lanlegipol.pl>
2023-08-04 09:44:42 +0200

    initialized backend and frontend of app

---
+++ backend/data/notes.js
@@ -0,0 +1,16 @@
+const notes = [
+    {
+        _id: "1",
+        hello: "hi",
+    }, 
+    {
+        _id: "2",
+        hello: "hi 2",
+    },
+    {
+        _id: "3",
+        hello: "hi 3",
+    },
+];
+
+module.exports = notes;
